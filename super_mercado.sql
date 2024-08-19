37e77165aaa55ac1a159239caea2406338632d04
jppardomontero <jppardo@marianosamaniego.edu.ec>
2024-08-19 10:09:28 -0500

    Evaluación

--- super_mercado.sql
+++ super_mercado.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-06-2024 a las 02:10:09
+-- Tiempo de generación: 19-08-2024 a las 17:01:25
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -32,12 +32,45 @@ CREATE TABLE `clientes` (
   `cedula` varchar(10) NOT NULL,
   `nombres` varchar(30) NOT NULL,
   `apellidos` varchar(30) NOT NULL,
-  `direccion` int(100) NOT NULL,
+  `direccion` varchar(100) NOT NULL,
   `sexo` varchar(10) NOT NULL,
   `correo` varchar(50) NOT NULL,
   `telefono` varchar(10) NOT NULL,
   `f_nacimiento` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id_clientes`, `cedula`, `nombres`, `apellidos`, `direccion`, `sexo`, `correo`, `telefono`, `f_nacimiento`) VALUES
+(9, '1104499288', 'Juan ', 'JIMENEZ ESPINOZA', 'NAPOLION BERRÚ Y JOSE ANGEL PALACIOS', 'Masculino', 'matinizaray@tecnologicosudamericano.edu.com', '(593) 98 0', '2024-07-11'),
+(10, '1104975816', 'Juan ', 'torres molina', 'BARRIO PADRE ESTEBAN', 'Masculino', 'matinizaray@tecnologicosudamericano.edu.com', '(593) 96 8', '2024-07-04'),
+(11, '1111111111', 'Juan ', 'RAMOS TITUANA', 'NAPOLION BERRÚ Y JOSE ANGEL PALACIOS', 'Femenino', 'aj_cueva@marianosamaniego.edu.ec', '(593) 98 0', '2024-07-11'),
+(14, '1103977623', 'Juan ', 'martinez', 'S/N', 'Masculino', 'aj_cueva@marianosamaniego.edu.ec', '(593) 96 8', '2024-07-06');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id_producto` int(10) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `precio` double NOT NULL,
+  `stock` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id_producto`, `nombre`, `precio`, `stock`) VALUES
+(1, 'Memorias ', 30, 100),
+(2, 'Fire stike pro', 75, 10),
+(3, 'Cargadores HP', 40, 50),
+(4, 'Laptop Hp', 300, 4);
 
 -- --------------------------------------------------------
 
@@ -75,6 +108,12 @@ ALTER TABLE `clientes`
   ADD UNIQUE KEY `cedula` (`cedula`);
 
 --
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id_producto`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -91,7 +130,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id_clientes` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_clientes` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id_producto` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -57,6 +57,37 @@ if (isset($_SESSION["login"]) && $_SESSION['login'] == 'activa') {
 <script src="vistas/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="vistas/dist/js/adminlte.min.js"></script>
+<!-- DataTables  & Plugins -->
+<script src="vistas/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="vistas/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="vistas/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="vistas/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="vistas/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="vistas/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="vistas/plugins/jszip/jszip.min.js"></script>
+<script src="vistas/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="vistas/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="vistas/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="vistas/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="vistas/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
 </body>
 
 </html>
